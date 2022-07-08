460b7319f6c075f331e872749870bff6b7359694
tonghs <tonghuashuai@gmail.com>
2022-07-09 00:57:58 +0800

    sonar 测试覆盖率

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -2,6 +2,7 @@
 
 DIR=$(pwd)
 
+# 子文件夹在先，根目录在最后，否则生成的 coverage.xml 有问题
 pytest -x -vv -s --cov=./bot --cov-report=xml:coverage-reports/coverage.xml $DIR/bot/tests/ \
     && pytest -x -vv -s --cov=./ --cov-append --cov-report=xml:coverage-reports/coverage.xml $DIR/tests/
 
