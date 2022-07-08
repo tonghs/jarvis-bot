5e904284efc1e9a7f053ef4b327495519570ce05
tonghs <tonghuashuai@gmail.com>
2022-07-09 00:51:19 +0800

    sonar 测试覆盖率

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -2,8 +2,8 @@
 
 DIR=$(pwd)
 
-pytest -x -vv -s --cov=. --cov-report=xml:coverage-reports/coverage.xml $DIR/tests/ \
-    && pytest -x -vv -s --cov=bot --cov-append --cov-report=xml:coverage-reports/coverage.xml $DIR/bot/tests/
+pytest -x -vv -s --cov=./bot --cov-report=xml:coverage-reports/coverage.xml $DIR/bot/tests/ \
+    && pytest -x -vv -s --cov=./ --cov-append --cov-report=xml:coverage-reports/coverage.xml $DIR/tests/
 
 #pytest_exit_code=$?
 #echo $pytest_exit_code
